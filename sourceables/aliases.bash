adcc5f7f39ba020f8c908c3f563a53935abd3544
Lucas Jenss <lucas@x3ro.de>
2013-02-20 15:58:56 +0100

    Oneline python smtp debug server, nice :)

--- sourceables/aliases.bash
+++ sourceables/aliases.bash
@@ -3,6 +3,10 @@ alias unhide='chflags nohidden'
 alias fact="elinks -dump randomfunfacts.com | sed -n '/^| /p' | tr -d \|"
 alias httpd="python -m SimpleHTTPServer 8081"
 
+# Debugging SMTP server
+# Found here: http://www.vurt.ru/2013/02/python-command-line-oneliners/
+alias smtpd="echo 'Starting debug SMTPD on port 25000' && sudo python -m smtpd -n -c DebuggingServer localhost:25000"
+
 # ===========
 # Git aliases
 # ===========
