8e97aba07986694afa3b2757acdd5f4396a3bd5a
Lucas Jenss <lucas@x3ro.de>
2013-03-13 23:23:50 +0100

    Finally fixed that annoying "perl error messages on remote servers" thing

--- sourceables/environment.bash
+++ sourceables/environment.bash
@@ -12,3 +12,8 @@ PHPSH_LIB_PATH=~/.dotfiles/installed/phpsh/lib/python2.7/site-packages
 PYTHONPATH="${PHPSH_LIB_PATH}:$PYTHONPATH"
 export PYTHONPATH
 
+# Fix those pesky perl warnings on remote servers, which is caused
+# by us sending weird locale settings
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
