7eda9d3ca972f5e6008ce46aa98ac42c3b663860
Lucas Jenss <lucas@x3ro.de>
2013-08-19 23:37:49 +0200

    A little PS1 adjustment

--- sourceables/ps1.bash
+++ sourceables/ps1.bash
@@ -2,4 +2,4 @@ parse_git_branch() {
     git branch 2> /dev/null | ansifilter | sed -e '/^[^*]/d' -e 's/*\(.*\) \([^)]*\)$/(\2) /'
 }
 
-export PS1="\[$(tput bold)\]\[$(tput setaf 46)\]ƒ \[$(tput setaf 27)\](\w) \[$(tput setaf 226)\]\$(parse_git_branch)\[$(tput setaf 255)\]\$ \[$(tput sgr0)\]";
+export PS1="\[$(tput bold)\]\[$(tput setaf 46)\]λ \[$(tput setaf 27)\](\w) \[$(tput setaf 226)\]\$(parse_git_branch)\[$(tput setaf 255)\]\$ \[$(tput sgr0)\]";
