bbdbff400689b0f3e7c35ef1e24062b1fb025315
Lucas Jenss <lucas@x3ro.de>
2013-10-25 15:10:02 +0200

    Make some noise if git bash completion is not loadable

--- sourceables/git-bash-completion.bash
+++ sourceables/git-bash-completion.bash
@@ -1,4 +1,7 @@
 # Git Bash Completion
-if [ -f /usr/local/etc/bash_completion.d/git-completion.bash ]; then
-    . /usr/local/etc/bash_completion.d/git-completion.bash
+GIT_COMPLETION_PATH="/usr/local/etc/bash_completion.d/git-completion.bash"
+if [ -f "$GIT_COMPLETION_PATH" ]; then
+    . "$GIT_COMPLETION_PATH"
+else
+    echo "Could not load git completion from '$GIT_COMPLETION_PATH'"
 fi
